44e771fff739563d1928f22cf23abde7e50342e7
Janek J <janek.j@survicate.com>
2023-08-17 13:14:50 +0200

    add prompt inptu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import OrganizationSearch from './components/OrganizationSearch';
+import PromptInput from './components/PromptInput';
 
 function App() {
   return (
     <div className="App">
       <h1>Organization Search</h1>
       <OrganizationSearch />
+      <PromptInput />
     </div>
   );
 }
