2dfa58f8f205d01ca716faf98c68af2fc5ed07d0
Tomasz Pawlisiak <tomasz.p@survicate.com>
2023-08-17 13:06:02 +0200

    Mock

--- src/components/OrganizationSearch.tsx
+++ src/components/OrganizationSearch.tsx
@@ -5,15 +5,18 @@ interface Organization {
   name: string;
 }
 
-const mockOrganizations = (chars: string) => {
+const mockOrganizations = (string: string) => {
+    const chars = string.slice(0, 12);
     const organizations = [];
 
     for (let i = 0; i < 10; i++) {
         organizations.push({
-            id: chars + Math.floor(Math.random() * (12 - chars.length / 2)).toString(),
+            id: chars + Math.random() * (12 - chars.length),
             name: `Organization ${i}`,
         });
     }
+
+    console.log(organizations);
 
     return organizations;
 }
@@ -27,17 +30,17 @@ const OrganizationSearch: React.FC = () => {
     setSearchText(inputValue);
 
     if (inputValue.length >= 3) {
-      try {
+    //   try {
         // const response = await fetch(`/api/organizations?query=${inputValue}`);
         // if (response.ok) {
           const matchingOrganizations = mockOrganizations(inputValue); //await response.json();
-          setSuggestions(matchingOrganizations.slice(0, 10));
+          setSuggestions(matchingOrganizations);
         // } else {
         //   console.error('Failed to fetch organizations');
         // }
-      } catch (error) {
-        console.error('Error fetching organizations', error);
-      }
+    //   } catch (error) {
+    //     console.error('Error fetching organizations', error);
+    //   }
     } else {
       setSuggestions([]);
     }
